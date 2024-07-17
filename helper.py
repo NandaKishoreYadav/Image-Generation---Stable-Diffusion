45f2c3cf43b01cab2419d3494db7a2aa7ec4b901
Nanda Kishore <98215088+NandaKishoreYadav@users.noreply.github.com>
2024-07-17 10:47:22 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -3,8 +3,8 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline 
 
 modelid = "CompVis/stable-diffusion-v1-4"
-device = "cuda"
-pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16) 
+device = "cpu"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float32) 
 pipe.to(device) 
 
 def generate_image(prompt):
