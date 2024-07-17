c2b9e984b7015d9973782f9fe51c1b7d27fb44f3
Nanda Kishore <98215088+NandaKishoreYadav@users.noreply.github.com>
2024-07-17 10:41:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from helper import generate_image
+from helper import *
 
 st.set_page_config(page_title="Image Generation",page_icon='📸')
 st.title('Image Generation')
 
 prompt = st.text_input("Enter the Prompt")
 
-image = generate_image(prompt)
+image = helper.generate_image(prompt)
 
 st.image(image, caption='Generated Image.', use_column_width=True)
